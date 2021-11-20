98b5f8e12a1c4dcbf5e0fd67a0f9dde845dd01b6
Johnny952 <jonathan.espinola@ug.uchile.cl>
2021-11-20 11:34:18 -0300

    changed gpu number

--- scripts/local_run_doicker.sh
+++ scripts/local_run_doicker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name carracing --rm --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=0 -it -v $(pwd):/home/user/workspace carracing /bin/bash
+docker run --name carracing --rm --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=5 -it -v $(pwd):/home/user/workspace carracing /bin/bash
