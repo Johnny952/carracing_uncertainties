c357a0aa4b3bee71a1253f638870b7805702d78d
Johnny952 <jonathan.espinola@ug.uchile.cl>
2022-03-25 15:02:24 -0300

    change weight decay in PPO and DDQN

--- src/dqn/components/uncert_agents/aleatoric.py
+++ src/dqn/components/uncert_agents/aleatoric.py
@@ -31,7 +31,7 @@ class AleatoricAgent(AbstactAgent):
             clip_grad=clip_grad,
         )
 
-        self._weight_decay = 1e-4
+        self._weight_decay = 1e-5
         self._model1 = Aleatoric(img_stack, len(actions)).to(self._device)
         self._model2 = Aleatoric(img_stack, len(actions)).to(self._device)
 
