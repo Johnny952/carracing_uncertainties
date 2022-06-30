791276b65d0ee6700ee838568fd8bb7c16359cba
Johnny952 <jonathan.espinola@ug.uchile.cl>
2022-06-30 14:17:09 -0400

    Modify noise test sh bootstraps parameters

--- src/ppo/noise_test.sh
+++ src/ppo/noise_test.sh
@@ -1,9 +1,9 @@
 python noise_test.py -M base
 python noise_test.py -M bnn -NN 25
-python noise_test.py -M bootstrap -NN 10
+python noise_test.py -M bootstrap -NN 5
 python noise_test.py -M dropout -NN 25
 python noise_test.py -M sensitivity -NN 100
 python noise_test.py -M vae
 python noise_test.py -M aleatoric
-python noise_test.py -M bootstrap2 -NN 10
+python noise_test.py -M bootstrap2 -NN 5
 python noise_test.py -M dropout2 -NN 25
